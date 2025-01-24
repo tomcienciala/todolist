18c5b00bf83676a36539ced6aadcb3efe126627c
Tomáš Cienciala <tomcienciala@gmail.com>
2025-01-24 21:47:26 +0100

    make Description and DueDate nullable in TaskEntity

--- ToDoList.Data/Entities/TaskEntity.cs
+++ ToDoList.Data/Entities/TaskEntity.cs
@@ -4,7 +4,7 @@ public class TaskEntity
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
-    public string Description { get; set; }
-    public DateTime DueDate { get; set; }
+    public string? Description { get; set; }
+    public DateTime? DueDate { get; set; }
     public bool IsCompleted { get; set; }
 }
